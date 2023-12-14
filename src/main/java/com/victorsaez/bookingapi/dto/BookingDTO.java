2dfaaccc8a95f95569b0ba38eecc29e9ea5360ad
Victor Saez <victor@lumahealth.io>
2023-12-14 01:27:31 -0300

    DTO's cleanup and fixed requirements

--- src/main/java/com/victorsaez/bookingapi/dto/BookingDTO.java
+++ src/main/java/com/victorsaez/bookingapi/dto/BookingDTO.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.Date;
 
@@ -17,21 +18,29 @@ import java.util.Date;
 public class BookingDTO {
 
     private Long id;
+
+    @NotNull(message = "startDate can't be null or empty")
     private Date startDate;
+    @NotNull(message = "startDate can't be null or empty")
     private Date endDate;
+
+    @NotNull(message = "status can't be null or empty")
     private BookingStatus status;
     private Double price;
 
     private Long clientId;
     private ClientDTO client;
 
+
     private Long propertyId;
     private PropertyDTO property;
 
+    @NotNull(message = "propertyId can't be null or empty")
     public Long getPropertyId() {
         return property != null ? property.getId() : propertyId;
     }
 
+    @NotNull(message = "clientId can't be null or empty")
     public Long getClientId() {
         return client != null ? client.getId() : clientId;
     }
--- src/main/java/com/victorsaez/bookingapi/dto/ClientDTO.java
+++ src/main/java/com/victorsaez/bookingapi/dto/ClientDTO.java
@@ -3,12 +3,15 @@ package com.victorsaez.bookingapi.dto;
 import com.victorsaez.bookingapi.entities.Client;
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientDTO {
 
     private Long id;
+    @NotNull(message = "Name can't be null or empty")
     private String name;
 
 }
